921ce5d88328654d16d7b052812c7c8f8ee9d850
victor Mendez <victormendez@unitec.edu>
2022-06-14 17:17:48 -0600

    {new} new design in login and main page

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { supabaseClient } from "./supabaseClient";
-import { Card, Form, Input, Button, message } from "antd";
+import { Card, Form, Input, Button, message, Image } from "antd";
 import { Usuarios } from "./Clients/Models/usuariosModel";
 import { GetUserByLogin } from "./api/userApi";
 import { Session } from "@supabase/supabase-js";
 import { useHistory } from "react-router-dom";
-
+import { Comment } from "./Clients/Models/commentModel";
 
 export default function Login() {
   const history = useHistory();
   const [usuario, setUsuario] = useState<Usuarios>();
   const [session, setSession] = useState<Session | null>(null);
+  const { Meta } = Card;
 
   const onFinish = async ({ usuario, password }: Usuarios) => {
     try {
@@ -18,7 +19,7 @@ export default function Login() {
         if (respone) {
           localStorage.setItem("myUser", respone.id);
           setUsuario(respone.data);
-          history.push('/');
+          history.push("/");
           message.success("ha iniciado sesion correctamente.");
         } else {
           message.error("usuario o contraseña incorrecta.");
@@ -30,51 +31,150 @@ export default function Login() {
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "50%",
-      }}
-    >
-      <Card>
+    <>
+      <div
+        style={{
+          background: "#2A3132",
+          display: "flex",
+          justifyContent: "end",
+          paddingTop: "20px",
+        }}
+      >
         <Form
           name="basic"
           labelCol={{ span: 12 }}
-          wrapperCol={{ span: 16 }}
+          wrapperCol={{ span: 14 }}
           initialValues={{ remember: true }}
           onFinish={onFinish}
           autoComplete="off"
         >
-          <Form.Item
-            label="Usuario"
-            name="usuario"
-            rules={[
-              {
-                required: true,
-                message: "Por favor ingrese su usuario!",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
+          <div style={{ display: "inline-flex" }}>
+            <h3
+              style={{
+                fontWeight: "bold",
+                color: "white",
+                paddingRight: "5px",
+                paddingTop: "3px",
+              }}
+            >
+              Usuario:{" "}
+            </h3>
+            <Form.Item
+              name="usuario"
+              rules={[
+                {
+                  required: true,
+                  message: "Por favor ingrese su usuario!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <h3
+              style={{
+                fontWeight: "bold",
+                color: "white",
+                paddingRight: "5px",
+                paddingTop: "3px",
+              }}
+            >
+              Contraseña:{" "}
+            </h3>
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+              
+            >
+              <Input.Password  />
+            </Form.Item>
 
-          <Form.Item
-            label="Contraseña"
-            name="password"
-            rules={[{ required: true, message: "Please input your password!" }]}
-          >
-            <Input.Password />
-          </Form.Item>
-
-          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit">
-              Iniciar sesión
-            </Button>
-          </Form.Item>
+            <Form.Item style={{ paddingRight: "25px" }}>
+              <Button type="primary"  htmlType="submit">
+                Iniciar sesión
+              </Button>
+            </Form.Item>
+          </div>
         </Form>
-      </Card>
-    </div>
+      </div>
+      <div
+        style={{
+          width: "100%",
+          position: "absolute",
+          display: "flex",
+          justifyContent: "center",
+          paddingTop: "25px",
+        }}
+      >
+        <div style={{ display: "inline-flex" }}>
+          <Card
+            hoverable
+            style={{ width: 320, height: 320, paddingLeft: "15px" }}
+            cover={
+              <Image
+                preview={{ visible: false }}
+                style={{ width: "100%" }}
+                src="https://fpnxfacxjubvouiewpad.supabase.co/storage/v1/object/public/images/IMG0_login.jpg?t=2022-06-14T22%3A52%3A18.106Z"
+              />
+            }
+          ></Card>
+
+          <Card
+            hoverable
+            style={{ width: 320, height: 320, paddingLeft: "15px" }}
+            cover={
+              <Image
+                preview={{ visible: false }}
+                style={{ width: "100%" }}
+                src="https://fpnxfacxjubvouiewpad.supabase.co/storage/v1/object/public/images/IMG2_login.jpg?t=2022-06-14T22%3A56%3A59.690Z"
+              />
+            }
+          ></Card>
+
+          <Card
+            hoverable
+            style={{ width: 320, height: 320, paddingLeft: "15px" }}
+            cover={
+              <Image
+                preview={{ visible: false }}
+                style={{ width: "100%" }}
+                src="https://fpnxfacxjubvouiewpad.supabase.co/storage/v1/object/public/images/IMG1_Login.jpg?t=2022-06-14T22%3A27%3A35.535Z"
+              />
+            }
+          ></Card>
+
+          <Card
+            hoverable
+            style={{ width: 320, height: 320, paddingLeft: "15px" }}
+            cover={
+              <Image
+                preview={{ visible: false }}
+                style={{ width: "100%" }}
+                src="https://fpnxfacxjubvouiewpad.supabase.co/storage/v1/object/public/images/IMG4_login.jpg?t=2022-06-14T22%3A52%3A47.163Z"
+              />
+            }
+          ></Card>
+        </div>
+      </div>
+
+      <div
+        style={{
+          background: "#2A3132",
+          width: "100%",
+          position: "absolute",
+          bottom: "0px",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <div style={{ display: "inline-flex", paddingTop: "15px" }}>
+          <p style={{ color: "white", paddingLeft: "15px" }}>
+            VirtualMall 2022 || Correo: virtualmall@gmail.Com || Telefono:
+            26480342
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
